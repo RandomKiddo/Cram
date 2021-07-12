6f9bf6ea46b5b1b8bb25afc610ebee0444d78ba6
RandomKiddo <nghugare2@outlook.com>
2021-07-12 10:53:21 -0400

    Finalized LZW Encoding

--- src/lossless/lzw.cpp
+++ src/lossless/lzw.cpp
@@ -1,1 +1,56 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+std::vector<int> encode(std::string str);
+
+int main(void) {
+    std::ifstream fin;
+    fin.open("/Users/firsttry/Desktop/Git/cram/master/src/test/lossless_lzw.txt");
+    std::ofstream fout;
+    fout.open("/Users/firsttry/Desktop/Git/cram/master/src/test/lossless_lzw_c.txt");
+    std::string buffer;
+    while (std::getline(fin, buffer)) {
+        std::vector<int> code = encode(buffer);
+        for (int i = 0; i < code.size(); ++i) {
+            fout << code.at(i) << " ";
+        }
+        fout << "\n";
+    }
+    fin.close();
+    fout.close();
+    return 0;
+}
+
+std::vector<int> encode(std::string str) {
+    std::unordered_map<std::string, int> table;
+    for (int i = 0; i < 256; ++i) {
+        std::string s = "";
+        s += char(i);
+        table[s] = i;
+    }
+    std::string p = "", c = "";
+    p += str[0];
+    int code = 256;
+    std::vector<int> output;
+    for (int i = 0; i < str.size(); ++i) {
+        if (i != str.size() - 1) {
+            c += str[i+1];
+        }
+        if (table.find(p+c) != table.end()) {
+            p += c;
+        } else {
+            //print
+            output.push_back(table[p]);
+            table[p+c] = code;
+            ++code;
+            p = c;
+        }
+        c = "";
+    }
+    //print
+    output.push_back(table[p]);
+    return output;
+}
